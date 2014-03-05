8df037cb3ff98170d24fb12338491b7f217b809a
Scott O'Malley <scottyomalley@gmail.com>
2014-03-05 11:52:52 +0000

    Split the layouts into seperate files added a list view

--- Dublin Transport/src/main/java/com/thehandsomecoder/dublintransport/cards/LuasCard.java
+++ Dublin Transport/src/main/java/com/thehandsomecoder/dublintransport/cards/LuasCard.java
@@ -53,9 +53,35 @@ public class LuasCard extends Card {
         });
     }
 
-    @Override
-    public void setupInnerViewElements(ViewGroup parent, View view) {
+   /* public void setupInnerViewElements(ViewGroup parent, View view) {
 
+        TextView textView = (TextView) view.findViewById(R.id.carddemo_googlenow_main_inner_lastupdate);
+        textView.setText("Update 14:57, 16 September"); //should use R.string.
+
+        StockListLayout list = (StockListLayout) view.findViewById(R.id.carddemo_googlenow_main_inner_list);
+        ListStockAdapter mAdapter = new ListStockAdapter(super.getContext(), buildArrayHelper());
+
+
+        list.setAdapter(mAdapter);
 
     }
+
+
+    //------------------------------------------------------------------------------------------
+
+
+    public ArrayList<Stock> buildArrayHelper() {
+
+        Stock s1 = new Stock("GOOG", 889.07f, 0.00f, 0.00f);
+        Stock s2 = new Stock("AAPL", 404.27f, 0.00f, 0.00f);
+        Stock s3 = new Stock("ENI", 17.59f, 0.06f, 0.34f);
+        Stock s4 = new Stock("Don Jones", 15.376f, 0.00f, 0.00f);
+
+        ArrayList<Stock> list = new ArrayList<Stock>();
+        list.add(s1);
+        list.add(s2);
+        list.add(s3);
+        list.add(s4);
+        return list;
+    }*/
 }
